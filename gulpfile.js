e0f3ab37d0c46409f0eec8ab89095a1bed098a7e
gerald guinto <geraldgold2016@gmail.com>
2024-11-10 10:45:29 +0100

    change name of custom_sass to main_sass for creation of custom sass when overriding main

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
+//added this for purging the css file
+const purgecss = require('gulp-purgecss');
+
 function buildStyles() {
-  return src('custom_sass/**/*.scss')
+  return src('main_sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    //added this method to purge the css file to make it lighter and use only the css that is needed(only use this when the project is ready to be deployed)
+    .pipe(purgecss({ content: ['*.html'] }))
+
     .pipe(dest('css'));
 }
 
 function watchTask() {
-  watch(['custom_sass/**/*.scss'], buildStyles);
+  //added this , '*.html' to the watch method to watch for changes in the html file(when purging the css file while editting)
+  watch(['main_sass/**/*.scss', '*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask); 
