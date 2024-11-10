afb7b3ddc8f5e3d51339d4215d30685124feb73c
gerald guinto <geraldgold2016@gmail.com>
2024-11-10 11:03:24 +0100

    added default keyword to ovveride the base styling and added custom sass to not mess the main_sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
-  return src('main_sass/**/*.scss')
+    //where the scss file is located
+  return src('custom_sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     //added this method to purge the css file to make it lighter and use only the css that is needed(only use this when the project is ready to be deployed)
     .pipe(purgecss({ content: ['*.html'] }))
@@ -15,7 +16,8 @@ function buildStyles() {
 
 function watchTask() {
   //added this , '*.html' to the watch method to watch for changes in the html file(when purging the css file while editting)
-  watch(['main_sass/**/*.scss', '*.html'], buildStyles);
+  //watch for changes in the custom_sass folder and the html file
+  watch(['custom_sass/**/*.scss', '*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask); 
